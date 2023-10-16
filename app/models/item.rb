09f12a4320b4161bf3b46244976762d161829cab
honami-f <hnmr.o.s2@gmail.com>
2023-10-16 22:54:36 +0900

    Active Storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,8 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_day
 
+  has_one_attached :image
+
   validates :name, :description, :price, presence: true
 
   validates :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"} 
