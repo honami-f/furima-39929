7b9800fee13708c800420b95c5a162baafc71f58
honami-f <hnmr.o.s2@gmail.com>
2023-10-19 00:56:40 +0900

    商品一覧表示の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  get "items/index"
   root to: "items#index"
-  resource :items, only: [:index, :new, :create]
+  resources :items, only: [:index, :new, :create]
 end
