80c555bfc751fbac1a1fd84c2bb45f762d2b12a8
honami-f <hnmr.o.s2@gmail.com>
2023-10-24 12:32:03 +0900

    コントローラー設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,32 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:create, :index]
+
+  def index
+    if !user_signed_in? || current_user.id == @item.user_id
+      redirect_to root_path
+    else
+      @order_payment = OrderPayment.new
+    end
+  end
+
+  def create
+    @order_payment = OrderPayment.new(order_params)
+    if @order_payment.valid?
+      @order_payment.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_payment).permit(:post_code, :prefecture, :city, :street_address, :building, :phone_number, :price).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
 end
--- app/models/order_payment.rb
+++ app/models/order_payment.rb
@@ -1,16 +1,17 @@
-class OederPayment
+class OrderPayment
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :post_code, :prefecture, :city, :street_address, :building, :phone_number
 
   with_options presence: true do
     validates :user_id, :item_id
-    validates :post_code, :phone_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :phone_number, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid."}
   end
   validates :prefecture, numericality: {other_than: 1, message: "can't be blank"}
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    Payment.create(order_id: order_id, item_id: item_id, order_id: order_id, post_code: post_code, prefecture: prefecture, city: city, street_address: street_address, building: building, phone_number: phone_number)
+    Payment.create(order_id: order.id, post_code: post_code, prefecture_id: prefecture, city: city, street_address: street_address, building: building, phone_number: phone_number)
   end
 
 end
