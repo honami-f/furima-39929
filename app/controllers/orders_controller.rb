8ae1cc97cac42b32c0d4d031b0848cae44968c64
honami-f <hnmr.o.s2@gmail.com>
2023-10-25 10:36:18 +0900

    カード決済機能導入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
   before_action :set_item, only: [:create, :index]
 
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     if !user_signed_in? || current_user.id == @item.user_id
       redirect_to root_path
     else
@@ -10,23 +11,34 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order_payment = OrderPayment.new(order_params)
+    @order_payment = OrderPayment.new(order_payment_params)
     if @order_payment.valid?
+      pay_item
       @order_payment.save
       redirect_to root_path
     else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render :index, status: :unprocessable_entity
     end
   end
 
   private
 
-  def order_params
-    params.require(:order_payment).permit(:post_code, :prefecture, :city, :street_address, :building, :phone_number, :price).merge(user_id: current_user.id, item_id: params[:item_id])
-  end
-
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def order_payment_params
+    params.require(:order_payment).permit(:post_code, :prefecture, :city, :street_address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: order_payment_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 
 end
--- app/models/order_payment.rb
+++ app/models/order_payment.rb
@@ -1,11 +1,12 @@
 class OrderPayment
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_code, :prefecture, :city, :street_address, :building, :phone_number
+  attr_accessor :user_id, :item_id, :post_code, :prefecture, :city, :street_address, :building, :phone_number, :token
 
   with_options presence: true do
     validates :user_id, :item_id
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :phone_number, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid."}
+    validates :token, presence: true
   end
   validates :prefecture, numericality: {other_than: 1, message: "can't be blank"}
 
