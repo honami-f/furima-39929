6d83b1a2580cd9313788a2ebec15c3dc77f15166
honami-f <hnmr.o.s2@gmail.com>
2023-10-25 20:55:01 +0900

    修正

--- app/models/order_payment.rb
+++ app/models/order_payment.rb
@@ -6,7 +6,7 @@ class OrderPayment
     validates :user_id, :item_id
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid' }
-    validates :city, :street_address, :token, presence: true
+    validates :city, :street_address, :token
   end
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
