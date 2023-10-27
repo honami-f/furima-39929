e2372879dec675a20cefe4a5c1c1da5b28d119be
honami-f <hnmr.o.s2@gmail.com>
2023-10-28 00:57:05 +0900

    変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.18.0'
 
 # Capistranoのログの表示に利用する
-set :application, 'furima_39929'
+set :application, 'furima-39929'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:honami-f/furima-39929.git'
