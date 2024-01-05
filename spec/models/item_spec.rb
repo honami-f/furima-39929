a2334ed3882d594fea3970145c94768c04d5e3a0
honami-f <hnmr.o.s2@gmail.com>
2024-01-05 22:33:13 +0900

    Update item_spec.rb

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe Item, type: :model do
       it '全ての項目の入力が存在すれば登録できる' do
         expect(@item).to be_valid
       end
+      
     end
     context '商品の出品ができない場合' do
       it '商品画像が空では登録できない' do
